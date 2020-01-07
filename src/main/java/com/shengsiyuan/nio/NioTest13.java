38eb1ee34c22ecc2915e82cad774863f5e9af17b
liu <liu@example.com>
2020-01-07 16:21:09 +0800

    1-7

--- src/main/java/com/shengsiyuan/nio/NioTest13.java
+++ src/main/java/com/shengsiyuan/nio/NioTest13.java
@@ -26,6 +26,7 @@ public class NioTest13 {
         FileChannel inputChannel = inputAccessFile.getChannel();
         FileChannel outputChannel = outputAccessFile.getChannel();
 
+        // 内存映射缓冲 MappedByteBuffer
         MappedByteBuffer inputData = inputChannel.map(FileChannel.MapMode.READ_ONLY, 0, length);
 
         System.out.println("=======================");
@@ -38,7 +39,9 @@ public class NioTest13 {
 
         Charset charset = StandardCharsets.UTF_8;
 
+        //解码：将磁盘上文件解析成字符或字符串
         CharsetDecoder charsetDecoder = charset.newDecoder();
+        //编码：转换成字节或字节数组
         CharsetEncoder charsetEncoder = charset.newEncoder();
 
         CharBuffer charBuffer = charsetDecoder.decode(inputData);
